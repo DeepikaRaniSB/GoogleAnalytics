5ea8f712a104d5227e2304982e5f92d63b36850c
DeepikaRaniSB <49220576+DeepikaRaniSB@users.noreply.github.com>
2025-05-21 04:18:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("continueBtn").addEventListener("click", function() {
+    alert("Continuing to the next step...");
+});
+
+function selectColor(color) {
+    alert("You selected " + color);
+}
